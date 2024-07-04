e1b47c4602526f21bb5192758691f54d34870689
AATHIVIGNESH18 <vigneshaathi4@gmail.com>
2024-07-04 12:18:06 +0530

    student-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { StudentFormComponent } from './student-form/student-form.component';
+
+interface Student {
+  name: string;
+  age: number;
+  gender: string;
+  course: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, StudentFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'student-form-app';
+  
 }
---
+++ src/app/student-form/student-form.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Student {
+  name: string;
+  age: any;
+  gender: string;
+  course: string;
+  email: string;
+}
+
+@Component({
+  selector: 'app-student-form',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './student-form.component.html',
+  styleUrl: './student-form.component.css'
+})
+export class StudentFormComponent {
+  students: Student[] = [];
+  currentStudent: Student = { name: '', age: null, gender: '', course: '', email: '' };
+  isEdit = false;
+  editIndex: number | null = null;
+
+  onSubmit(form: any) {
+    if (this.isEdit && this.editIndex !== null) {
+      this.students[this.editIndex] = { ...this.currentStudent };
+    } else {
+      this.students.push({ ...this.currentStudent });
+    }
+    this.resetForm(form);
+  }
+
+  editStudent(index: number) {
+    this.currentStudent = { ...this.students[index] };
+    this.isEdit = true;
+    this.editIndex = index;
+  }
+
+  deleteStudent(index: number) {
+    this.students.splice(index, 1);
+    if (this.editIndex === index) {
+      this.resetForm();
+    }
+  }
+
+  resetForm(form?: any) {
+    this.currentStudent = { name: '', age: null, gender: '', course: '', email: '' };
+    this.isEdit = false;
+    this.editIndex = null;
+    if (form) {
+      form.resetForm();
+    }
+  }
+}
